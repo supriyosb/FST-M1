a39491cd1b9657d72129ce4db7d5b27f3c66861a
supriyosb <32214345+supriyosb@users.noreply.github.com>
2024-06-15 21:46:06 +0530

    Added SQL files as part of FST training

---
+++ SQL/Activities/Activity1.sql
@@ -0,0 +1,10 @@
+REM   Script: SQL Activities
+REM   SQL Activities
+
+CREATE TABLE salesman ( 
+    salesman_id int, 
+    salesman_name varchar2(32), 
+    salesman_city varchar2(32), 
+    commission int 
+);
+
---
+++ SQL/Activities/Activity10.sql
@@ -0,0 +1,81 @@
+REM   Script: Activity10
+REM   Activity10
+
+CREATE TABLE salesman ( 
+    salesman_id int, 
+    salesman_name varchar2(32), 
+    salesman_city varchar2(32), 
+    commission int 
+);
+
+INSERT INTO salesman VALUES(5002, 'Nail Knite', 'Paris', 13);
+
+INSERT ALL 
+    INTO salesman VALUES(5005, 'Pit Alex', 'London', 11) 
+    INTO salesman VALUES(5006, 'McLyon', 'Paris', 14) 
+    INTO salesman VALUES(5007, 'Paul Adam', 'Rome', 13) 
+    INTO salesman VALUES(5003, 'Lauson Hen', 'San Jose', 12) 
+SELECT 1 FROM DUAL;
+
+SELECT * FROM salesman;
+
+SELECT salesman_id, salesman_city FROM salesman;
+
+SELECT * FROM salesman WHERE salesman_city='Paris';
+
+SELECT salesman_id, commission FROM salesman WHERE salesman_name='Paul Adam';
+
+SELECT salesman_id, salesman_city FROM salesman;
+
+SELECT * FROM salesman WHERE salesman_city='Paris';
+
+SELECT salesman_id, commission FROM salesman WHERE salesman_name='Paul Adam';
+
+ALTER TABLE salesman ADD grade int;
+
+UPDATE salesman SET grade=100;
+
+SELECT * FROM salesman;
+
+select distinct salesman_id from orders;
+
+select order_no, order_date from orders order by order_date;
+
+select order_no, purchase_amount from orders order by purchase_amount DESC;
+
+select * from orders where purchase_amount < 500;
+
+select * from orders where purchase_amount between 1000 and 2000;
+
+select SUM(purchase_amount) AS "Total sum" from orders;
+
+select AVG(purchase_amount) AS "Average" from orders;
+
+select MAX(purchase_amount) AS "Maximum" from orders;
+
+select MIN(purchase_amount) AS "Minumum" from orders;
+
+select COUNT(distinct salesman_id) AS "Total count" from orders;
+
+SELECT customer_id, MAX(purchase_amount) AS "Max Amount" FROM orders GROUP BY customer_id;
+
+SELECT salesman_id, order_date, MAX(purchase_amount) AS "Max Amount" FROM orders  
+WHERE order_date=To_DATE('2012/08/17', 'YYYY/MM/DD') GROUP BY salesman_id, order_date;
+
+SELECT customer_id, order_date, MAX(purchase_amount) AS "Max Amount" FROM orders 
+GROUP BY customer_id, order_date 
+HAVING MAX(purchase_amount) IN(2030, 3450, 5760, 6000);
+
+SELECT * FROM orders 
+WHERE salesman_id=(SELECT DISTINCT salesman_id FROM orders WHERE customer_id=3007);
+
+SELECT * FROM orders 
+WHERE salesman_id IN (SELECT salesman_id FROM salesman WHERE city='New York');
+
+SELECT grade, COUNT(*) FROM customers 
+GROUP BY grade HAVING grade>(SELECT AVG(grade) FROM customers WHERE city='New York');
+
+SELECT order_no, purchase_amount, order_date, salesman_id FROM orders 
+WHERE salesman_id IN( SELECT salesman_id FROM salesman 
+WHERE commission=( SELECT MAX(commission) FROM salesman));
+
